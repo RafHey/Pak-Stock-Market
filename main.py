8d7e02290676954fca9e62347d761a5dfa88b952
unknown <rafehhameed@gmail.com>
2021-12-25 18:58:19 +0500

    Tested User Add Stock!

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from user import User
 
 def main():
-    a = User("Testing")
-    a.add_stock("TRG",200)
+    a = User("Raf")
+    a.add_stock("TRG",100)
+    a.add_stock('UNITYR3',2500)
     #a.add_stock("BGL",100)
     #a.add_stock("GGGL",50)
     a.save_user_to_file()
